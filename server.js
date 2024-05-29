2044591f084bc6609299703ec39dadfc7c4dd90b
yamada <yamada@dig>
2024-05-29 17:56:12 +0900

    マイグレまでやっったよう
    Co-authored-by: ngtnok <ngtnok@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const config = require("./knexfile");
+const environment = process.env.DATABASE_URL ? "production" : "development";
+const knex = require("knex")(config[environment]);
+
+// module.exports = knex(config[environment]);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`listening on port : ${PORT}`));
