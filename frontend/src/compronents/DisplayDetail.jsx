b2a27a94e95918d8c605f95aeab85b5c38007cc5
kazuma_okada <kazuma_okada@mail.toyota.co.jp>
2024-05-29 18:50:20 +0900

    add : コンポーネントの追加

---
+++ frontend/src/compronents/DisplayDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DisplayDetail = (props) => {
+    const {trashId} = props;
+
+    //ゴミの捨て場所とかをfetchする
+
+  return (
+    <div>DisplayDetail</div>
+  )
+}
+
+export default DisplayDetail
