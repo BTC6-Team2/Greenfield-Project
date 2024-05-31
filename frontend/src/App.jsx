866a3f33b62e2d4c0fda8d6d35145ca49ab2440c
MASAru0115 <masaru.matsuda@mail.toyota-body.co.jp>
2024-05-31 15:18:01 +0900

    add:サインイン画面追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,31 @@ import Header from "./compronents/Header";
 import SearchBar from "./compronents/SearchBar";
 import DisplayResult from "./compronents/DisplayResult";
 import DisplayDetail from "./compronents/DisplayDetail";
+import SignIn from "./compronents/SignIn";
 
 const App = () => {
-  const [searchWord, setSearchWord] = useState("");
+    const [searchWord, setSearchWord] = useState("");
+    const [success, setSuccess] = useState(false);
 
-  return (
-    <div>
-      <Header />
-      <SearchBar setSearchWord={setSearchWord} />
-      <DisplayResult searchWord={searchWord} />
-      {/* <DisplayDetail /> */}
-    </div>
-  );
+    return (
+        <div>
+            {!success ? (
+                <SignIn setSuccess={setSuccess}></SignIn>
+            ) : (
+                <div>
+                    <Header />
+                    <SearchBar setSearchWord={setSearchWord} />
+                    <DisplayResult searchWord={searchWord} />
+                    {/* <DisplayDetail /> */}
+                </div>
+            )}
+
+            {/* <Header />
+            <SearchBar setSearchWord={setSearchWord} />
+            <DisplayResult searchWord={searchWord} />
+            <DisplayDetail /> */}
+        </div>
+    );
 };
 
 export default App;
