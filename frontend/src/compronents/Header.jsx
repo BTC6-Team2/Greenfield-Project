50d79a86743d96fff1b7c3ad594498c5260c1b87
MASAru0115 <masaru.matsuda@mail.toyota-body.co.jp>
2024-06-04 13:07:54 +0900

    add:ログアウト /からsigninへのリダイレクト

--- frontend/src/compronents/Header.jsx
+++ frontend/src/compronents/Header.jsx
@@ -1,9 +1,21 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 const Header = () => {
+    const logout = () => {
+        fetch("http://localhost:3000/logout").then((res) => {
+            console.log("logout処理できてるか res :", res.ok);
+        });
+    };
     return (
         <header className="header">
             <h2>ごみ出しアプリ</h2>
+            <div className="logout">
+                {/* <button onClick={logout}>Logout</button> */}
+                <button>
+                    <a href="/logout">ログアウト</a>
+                </button>
+            </div>
         </header>
     );
 };
